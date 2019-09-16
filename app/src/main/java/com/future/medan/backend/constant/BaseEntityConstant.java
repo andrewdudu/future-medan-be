a53b98586d5c9275e2d000109e27b3623c80d287
Andrew Wijaya <jun.quan.mail@gmail.com>
2019-09-16 22:27:42 +0700

    add BaseEntity with constants

---
+++ app/src/main/java/com/future/medan/backend/constant/BaseEntityConstant.java
@@ -0,0 +1,9 @@
+package com.future.medan.backend.constant;
+
+public class BaseEntityConstant {
+
+    public static final String ID = "id";
+    public static final String VERSION = "version";
+    public static final String SYSTEM_UUID = "system-uuid";
+    public static final String STRATEGY_UUID2 = "uuid2";
+}
---
+++ app/src/main/java/com/future/medan/backend/model/entity/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.future.medan.backend.model.entity;
+
+import com.future.medan.backend.constant.BaseEntityConstant;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Data
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+public class BaseEntity {
+
+    @Id
+    @Column(name = BaseEntityConstant.ID)
+    @GeneratedValue(generator = BaseEntityConstant.SYSTEM_UUID)
+    @GenericGenerator(name = BaseEntityConstant.SYSTEM_UUID, strategy = BaseEntityConstant.STRATEGY_UUID2)
+    private String id;
+}
