7cb198ec3f4c3ea422fe1fdb637e99a2daf5007a
Andrew Wijaya <jun.quan.mail@gmail.com>
2019-09-19 19:54:33 +0700

    Add get all product API (controller, repo, service with impl)

---
+++ app/src/main/java/com/future/medan/backend/services/ProductService.java
@@ -0,0 +1,10 @@
+package com.future.medan.backend.services;
+
+import com.future.medan.backend.models.entity.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> getAll();
+}
