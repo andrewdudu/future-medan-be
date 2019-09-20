3ce25b1533de7710a7795f83da2cb983eb322839
Andrew Wijaya <jun.quan.mail@gmail.com>
2019-09-20 16:14:45 +0700

    Remove unused constant

--- app/src/main/java/com/future/medan/backend/models/constants/BaseEntityConstant.java
+++ app/src/main/java/com/future/medan/backend/models/constants/BaseEntityConstant.java
@@ -3,7 +3,6 @@ package com.future.medan.backend.models.constants;
 public class BaseEntityConstant {
 
     public static final String ID = "id";
-    public static final String VERSION = "version";
     public static final String SYSTEM_UUID = "system-uuid";
     public static final String STRATEGY_UUID2 = "uuid2";
 }
