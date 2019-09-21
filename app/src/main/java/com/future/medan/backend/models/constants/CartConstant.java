a64e4ddf1663d9de92a6cf6f1b513acfab16e79a
Andrew Wijaya <jun.quan.mail@gmail.com>
2019-09-21 19:59:35 +0700

    Fix typo

--- app/src/main/java/com/future/medan/backend/models/constants/CartConstant.java
+++ app/src/main/java/com/future/medan/backend/models/constants/CartConstant.java
@@ -5,5 +5,5 @@ public class CartConstant {
     public static final String CART_QTY = "qty";
     public static final String CART_USER_ID = "user_id";
     public static final String CART_ID = "cart_id";
-    public static final String CART_PRODUCT_ID = "product_id";
+    public static final String PRODUCT_ID = "product_id";
 }
