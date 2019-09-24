955c2804213c852d4b5dc71a994d3591578625f3
Syntia <syntiaaa00@gmail.com>
2019-09-24 16:18:56 +0700

    Added Admin to model

---
+++ app/src/main/java/com/future/medan/backend/models/constants/AdminConstant.java
@@ -0,0 +1,7 @@
+package com.future.medan.backend.models.constants;
+
+public class AdminConstant {
+    public static final String ADMIN_USERNAME = "username";
+    public static final String ADMIN_EMAIL = "email";
+    public static final String ADMIN_PASSWORD = "password";
+}
