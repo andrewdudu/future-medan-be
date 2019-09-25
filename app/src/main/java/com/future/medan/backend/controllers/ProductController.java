a6cdf0ef50aec9a91ac3a8b500d51552effafc00
Andrew Wijaya <jun.quan.mail@gmail.com>
2019-09-25 14:01:23 +0700

    Add ProductWebResponse

--- app/src/main/java/com/future/medan/backend/controllers/ProductController.java
+++ app/src/main/java/com/future/medan/backend/controllers/ProductController.java
@@ -4,6 +4,7 @@ import com.future.medan.backend.models.constants.ApiPath;
 import com.future.medan.backend.models.entity.Product;
 import com.future.medan.backend.responses.Response;
 import com.future.medan.backend.responses.ResponseHelper;
+import com.future.medan.backend.responses.WebResponseConstructor;
 import com.future.medan.backend.services.ProductService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.stream.Collectors;
 
 @Api
 @RestController
@@ -28,7 +29,10 @@ public class ProductController {
 
     @GetMapping(ApiPath.PRODUCTS)
     public Response getAll() {
-        return ResponseHelper.ok(productService.getAll());
+        return ResponseHelper.ok(productService.getAll()
+                .stream()
+                .map(product -> WebResponseConstructor.toWebResponse(product))
+                .collect(Collectors.toList()));
     }
 
     @PostMapping(value = ApiPath.PRODUCTS, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
