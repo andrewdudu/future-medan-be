0660248317efb0aa1919f41feb253977d0bd06ab
Syntia <syntiaaa00@gmail.com>
2019-10-05 16:13:55 +0700

    git add role enum

---
+++ app/src/main/java/com/future/medan/backend/models/enums/RoleEnum.java
@@ -0,0 +1,12 @@
+package com.future.medan.backend.models.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum RoleEnum {
+    USER,
+    MERCHANT,
+    ADMIN
+}
