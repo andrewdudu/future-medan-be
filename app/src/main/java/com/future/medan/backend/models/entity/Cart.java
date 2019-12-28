5ee5a9b1c3e353ce590d9d82eb16a6429a2c1105
Andrew Wijaya <jun.quan.mail@gmail.com>
2019-12-28 14:54:56 +0700

    remove unused import

--- app/src/main/java/com/future/medan/backend/models/entity/Cart.java
+++ app/src/main/java/com/future/medan/backend/models/entity/Cart.java
@@ -1,19 +1,12 @@
 package com.future.medan.backend.models.entity;
 
-import com.future.medan.backend.constants.BaseEntityConstant;
 import com.future.medan.backend.constants.CartConstant;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.UpdateTimestamp;
-import org.hibernate.validator.constraints.UniqueElements;
 
 import javax.persistence.*;
-import java.util.Date;
-import java.util.HashSet;
 import java.util.Set;
 
 @Data
