46634c35d3fa1dfd537a8fbfd537d6aea9676860
syntialai <53588149+syntialai@users.noreply.github.com>
2020-01-14 08:01:02 +0700

    Review : add unit test for controller Sequence : add unit test for sequence service Storage : add unit test for storage service

--- app/src/test/java/com/future/medan/backend/services/impl/SequenceImplTests.java
+++ app/src/test/java/com/future/medan/backend/services/impl/SequenceImplTests.java
@@ -18,8 +18,8 @@ public class SequenceImplTests {
 
     private SequenceService sequenceService;
 
-    private Sequence sequence;
-    private String key;
+    private Sequence sequence, sequenceNew;
+    private String key, key2;
 
     @Before
     public void setup() {
@@ -28,7 +28,9 @@ public class SequenceImplTests {
         this.sequenceService = new SequenceServiceImpl(sequenceRepository);
 
         this.sequence = new Sequence();
-        this.key = "11";
+        this.sequenceNew = null;
+        this.key = "SU";
+        this.key2 = "NU";
 
         sequence.setKey(key);
         sequence.setValue(11);
@@ -36,10 +38,24 @@ public class SequenceImplTests {
 
     @Test
     public void save_Ok() {
-        String expected = "11-0012";
+        String expected = "SU-0012";
         when(sequenceRepository.findByKey(key)).thenReturn(sequence);
         when(sequenceRepository.save(sequence)).thenReturn(sequence);
 
         assertEquals(expected, sequenceService.save(key));
+    }
+
+    @Test
+    public void save_SequenceNull() {
+        String expected = "NU-0001";
+        when(sequenceRepository.findByKey(key2)).thenReturn(sequenceNew);
+
+        sequenceNew = new Sequence();
+        sequenceNew.setKey(key2);
+        sequenceNew.setValue(0);
+        when(sequenceRepository.save(sequenceNew)).thenReturn(sequenceNew);
+
+        String actual = sequenceService.save(key2);
+        assertEquals(expected, actual);
     }
 }
